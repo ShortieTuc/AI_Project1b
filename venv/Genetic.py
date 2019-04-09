b371fddcc9c38846e16b4fed4dff186b49b7de15
ibalampanis <ibalampanis@isc.tuc.gr>
2019-04-10 02:29:19 +0300

    Liampos

--- venv/Genetic.py
+++ venv/Genetic.py
@@ -168,7 +168,7 @@ population = create_starting_population(population_size, chromosome_length_x, ch
 
 # Make Hard Constraint Check
 check_table = feasibility_check(population, population_size, chromosome_length_x, chromosome_length_y)
-"""
+
 print(check_table)
 count = 0
 for i in range(population_size):
@@ -176,10 +176,10 @@ for i in range(population_size):
         print(i)
         count += 1
 print('\n', count, '\n')
-"""
+
 # Make Soft Constraint Check and Calculate Score
 score_table = fitness_check(check_table, population, population_size, chromosome_length_x, chromosome_length_y)
-"""
+
 print(score_table)
 count = 0
 for i in range(population_size):
@@ -187,6 +187,6 @@ for i in range(population_size):
         print(i)
         count += 1
 print('\n', count, '\n')
-"""
+
 
 
